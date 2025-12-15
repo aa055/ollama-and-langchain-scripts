8475afc8d5e4c3725178e7b797e4d9fe3436862f
aa055 <aamirmoiz531@outlook.com>
2025-12-15 22:30:56 +0400

    Enhance function docstrings and update model parameters for clarity

--- ollama_with_langchain/ollama_model_tool_use.py
+++ ollama_with_langchain/ollama_model_tool_use.py
@@ -13,6 +13,9 @@ def validate_user(user_id: int, addresses: List[str]) -> bool:
     Args:
         user_id (int): the user ID.
         addresses (List[str]): Previous addresses as a list of strings.
+
+    Returns:
+        bool: True if the user is validated, False otherwise.
     """
 
     return True
@@ -23,6 +26,9 @@ def addition(numbers: List[int]) -> int:
     
     Args:
         numbers (List[int]): A list of integers to add.
+
+    Returns:
+        int: The sum of the numbers.
     """
 
     return sum(numbers)
@@ -34,8 +40,11 @@ def subtraction(a: int, b: int) -> int:
     Args:
         a (int): The number to subtract from.
         b (int): The number to subtract.
+    
+    Returns:
+        int: The result of the subtraction.
     """
-
+    
     return a - b
 
 @tool
@@ -45,6 +54,9 @@ def multiplication(a: int, b: int) -> int:
     Args:
         a (int): The first number.
         b (int): The second number.
+
+    Returns:
+        int: The product of the two numbers.
     """
 
     return a * b
@@ -56,6 +68,9 @@ def division(a: int, b: int) -> float:
     Args:
         a (int): The numerator.
         b (int): The denominator.
+
+    Returns:
+        float: The result of the division.
     """
 
     if b == 0:
@@ -64,10 +79,11 @@ def division(a: int, b: int) -> float:
 
 
 llm = ChatOllama(
-    model="llama3.2",
+    model="qwen3:4b",
     validate_model_on_init=True,
-    temperature=0,
-).bind_tools([validate_user, addition, subtraction, multiplication, division])
+    temperature=0.7,
+    # reasoning=True
+).bind_tools([addition, subtraction, multiplication, division])
 
 # # Test user validation
 # result = llm.invoke(
@@ -80,7 +96,7 @@ llm = ChatOllama(
 
 # Test arithmetic operations
 result = llm.invoke(
-    "Could you add the numbers 5, 10, and 15?"
+    "Could you add the numbers 5, 10, and 15? "
 )
 
 print(result)
@@ -88,4 +104,12 @@ print(result)
 if isinstance(result, AIMessage) and result.tool_calls:
     print(result.tool_calls)
 
+## Try out with multiple tool calls in a single response by asking to perform several math operations.
 
+# result = llm.invoke(
+#     "Could you add the numbers 5, 10, and 15? Then subtract 8 from the result, and multiply that by 2. "
+# )
+# # the result will not contain the final answer directly, but will have tool calls to perform each step
+# # for that, we need to execute each tool call in sequence and feed the results back to the model 
+
+## Try it out in a loop like in ollama_with_langchain/agent_loop.py
